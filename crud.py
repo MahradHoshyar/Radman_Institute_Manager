852d5c27abcfcd1f2e140754e26a772b73c22dd0
Mojishoon <mojtaba8203motameni@gmail.com>
2024-10-20 16:12:52 +0330

    added crud file with creat, read, update and delete  oprations for users in it.

--- crud.py
+++ crud.py
@@ -10,12 +10,7 @@ import schemas
 # -------------------------------------------------------------------------------------------------------
 
 def create_user(db: Session(engine), user: schemas.UserCreate):
-    db_user = models.User(id=user.id, gender=user.gender, first_name=user.first_name, last_name=user.last_name,
-                          father_name=user.father_name, date_of_birth=user.date_of_birth,
-                          national_code=user.national_code, phone_number=user.phone_number, role_id=user.role_id,
-                          recruitment_date=user.recruitment_date, is_super_admin=user.is_super_admin,
-                          is_panel_user=user.is_panel_user, permission_group_id=user.permission_group_id,
-                          is_enabled=user.is_enabled, recorder_id=user.recorder_id)
+    db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -23,26 +18,20 @@ def create_user(db: Session(engine), user: schemas.UserCreate):
 
 
 def get_user(db: Session(engine), user_id: int):
-    db_user = db.scalars(select(models.User).where(models.User.id == user_id))
-    if not [*db_user]:
-        return None
-    return db.scalars(select(models.User).where(models.User.id == user_id)).one()
+    return db.scalars(select(models.User).where(models.User.id == user_id, models.User.is_enabled)).first()
 
 
 def get_user_by_last_name(db: Session(engine), user_last_name: str):
-    db_user = db.scalars(select(models.User).where(models.User.last_name == user_last_name))
-    if not [*db_user]:
-        return None
-    return db.scalars(select(models.User).where(models.User.last_name == user_last_name)).one()
+    return db.scalars(select(models.User).where(models.User.last_name == user_last_name,
+                                                models.User.is_enabled)).first()
 
 
 def get_users(db: Session(engine)):
-    return db.scalars(select(models.User))
+    return db.scalars(select(models.User).where(models.User.is_enabled))
 
 
-def update_user(db: Session(engine), user: schemas.User):
-    db_user = db.scalars(select(models.User).where(models.User.id == user.id)).one()
-    db_user.id = user.id
+def update_user(db: Session(engine), user: schemas.User, user_id: int):
+    db_user = db.scalars(select(models.User).where(models.User.id == user_id)).first()
     db_user.gender = user.gender
     db_user.first_name = user.first_name
     db_user.last_name = user.last_name
@@ -62,7 +51,7 @@ def update_user(db: Session(engine), user: schemas.User):
 
 
 def delete_user_by_id(db: Session(engine), user_id: int):
-    db_user = db.get(models.User, user_id)
-    db.delete(db_user)
+    db_user = db.scalars(select(models.User).where(models.User.id == user_id)).one()
+    db_user.is_enabled = False
     db.commit()
     return db_user
